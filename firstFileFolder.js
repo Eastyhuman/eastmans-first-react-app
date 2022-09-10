c14d0a4ee9913ed3a86d16f2df06a933c4ba34fa
E C <ec@Es-MacBook-Pro.local>
2022-09-10 13:12:38 -0500

    doubling up on log changes

--- firstFileFolder.js
+++ firstFileFolder.js
@@ -1,3 +1,4 @@
 console.log("Hello World")
 console.log("josh has to make this very difficult doesn't he")
 console.log("help i'm stuck trying to learn coding")
+console.log("help i'm stuck trying to learn coding")
--- secondFileFolder.js
+++ secondFileFolder.js
@@ -1,3 +1,4 @@
 console.log("hello beautiful")
 console.log("what in the world is going on")
 console.log("Help Eastman learn the coding fast josh")
+console.log("Help Eastman learn the coding fast josh")
